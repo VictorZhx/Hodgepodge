076c57bf13da28a18fe131c2a8cd55f7a9f07ac7
bjzhangxuan <zhangxuaninfo@jd.com>
2018-01-03 15:16:45 +0800

    添加aac

---
+++ app/src/main/java/com/victor/hodgepodge/AACActivity.java
@@ -0,0 +1,10 @@
+package com.victor.hodgepodge;
+
+import android.app.Activity;
+
+/**
+ * Created by zhangxuaninfo on 2017/11/20.
+ */
+
+public class AACActivity extends Activity {
+}
---
+++ app/src/main/java/com/victor/hodgepodge/AACFragment.java
@@ -0,0 +1,10 @@
+package com.victor.hodgepodge;
+
+import android.app.Fragment;
+
+/**
+ * Created by zhangxuaninfo on 2017/11/20.
+ */
+
+public class AACFragment extends Fragment {
+}
